097e331b0fa287a6507c8e58ac7314670443f585
heir <71329348+he1r@users.noreply.github.com>
2022-02-18 18:56:37 +0100

    remaking the layout

---
+++ Includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Css/style.css">
+    <title>LOCAL WEB</title>
+</head>
---
+++ LogIn/index.php
@@ -0,0 +1,22 @@
+<?php
+include '../Includes/header.php';
+?>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 mx-auto py-4 px-0">
+            <div class="card p-0">
+                <div class="card-title text-center">
+                    <h5 class="mt-5">LOG IN</h5>
+                </div>
+                <form class="signup">
+                    <div class="form-group"><input type="text" class="form-control" placeholder="Username"></div>
+                    <div class="invalid-feedback">Please choose a username.</div>
+                    <div class="form-group"><input type="password" class="form-control" placeholder="password"></div> <button type="button" class="btn btn-primary">Login</button>
+                </form>
+                <a href="#"><p class="text-center pt-2 mr-1">Sign Up</p></a> 
+            </div>
+        </div>
+    </div>
+</div>
+</body>
