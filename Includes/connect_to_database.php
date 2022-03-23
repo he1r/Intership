4cb9db0254254cfd4d76579efb9ffda7ec0fa7a9
heir <71329348+he1r@users.noreply.github.com>
2022-03-23 15:34:14 +0100

    Update connect_to_database.php

--- Includes/connect_to_database.php
+++ Includes/connect_to_database.php
@@ -1,5 +1,5 @@
 <?php
-$dbcon = new mysqli("127.0.0.1", "root", "", "localweb");
+$dbcon = new mysqli("localhost", "root", "", "localweb");
     if ($dbcon -> connect_errno) {
         echo "Failed to connect to MySQL: " . $dbcon -> connect_error;
         exit;
