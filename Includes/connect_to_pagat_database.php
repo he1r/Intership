b258a47cebe7788b8c44dce58997ad8e9f09114e
heir <71329348+he1r@users.noreply.github.com>
2022-03-23 15:33:51 +0100

    Update connect_to_pagat_database.php

--- Includes/connect_to_pagat_database.php
+++ Includes/connect_to_pagat_database.php
@@ -1,5 +1,5 @@
 <?php
-$dbcon = new mysqli("127.0.0.1", "root", "", "test_paga");
+$dbcon = new mysqli("localhost", "root", "", "test_paga");
     if ($dbcon -> connect_errno) {
         echo "Failed to connect to MySQL: " . $dbcon -> connect_error;
         exit;
